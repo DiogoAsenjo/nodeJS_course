f3cc64d6abd13a3daca5e5afe76f5749c61edd71
Diogo Asenjo <diogoasenjo@outlook.com>
2023-07-06 07:59:43 -0300

    Ajustes

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -3,4 +3,5 @@ const fs = require("fs");
 /*const hello = "Hello World"; ///Para rodar o arquivo no terminal, basta abrir o terminal e dar o comando node 'nome.extensão' do arquivo.
 console.log(hello);*/
 
-fs.readFileSync();
+const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
+console.log(textIn);
