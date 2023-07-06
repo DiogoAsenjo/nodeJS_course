41c0a8ec90484bb29457df3d0dd66fdeea97b5ac
Diogo Asenjo <diogoasenjo@outlook.com>
2023-07-06 10:24:51 -0300

    Routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ///////////////////////////////////////
 ////FILES
@@ -39,8 +40,22 @@ console.log("Will read file!"); //Aqui como não depende da linha anterior, enqu
 ///////////////////////////////////////
 ////SERVER
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
-});
+  console.log(req.url);
+
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview!");
+  } else if (pathName === "/product") {
+    res.end("This is the product!");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello -world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+}); //Começando a entender Routing.
 
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
