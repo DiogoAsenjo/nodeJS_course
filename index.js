765e4ad2f3fc35ea51360e767b5f5acd5ef5dba5
Diogo Asenjo <diogoasenjo@outlook.com>
2023-07-06 07:41:58 -0300

    Aula Using Modules

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+
+/*const hello = "Hello World"; ///Para rodar o arquivo no terminal, basta abrir o terminal e dar o comando node 'nome.extensão' do arquivo.
+console.log(hello);*/
+
+fs.readFileSync();
