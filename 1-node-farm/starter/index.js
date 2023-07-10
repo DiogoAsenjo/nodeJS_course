54b134d4ecda43ef94e945c0b7d708f4a8f21da9
Diogo Asenjo <diogoasenjo@outlook.com>
2023-07-10 15:20:50 -0300

    Preenchendo os templates

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -39,23 +39,62 @@ console.log("Will read file!"); //Aqui como não depende da linha anterior, enqu
 
 ///////////////////////////////////////
 ////SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObject = JSON.parse(data); //Aqui e na linha de cima foi criada uma API para ler o arquivo .json e conseguir usar ele em javascript.
+
+const dataObj = JSON.parse(data); //Aqui e na linha de cima foi criada uma API para ler o arquivo .json e conseguir usar ele em javascript.
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
   const pathName = req.url;
-
+  //Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the overview!");
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join(""); //Aqui ele está basicamente lendo qual dos cards que irá aparecer. Entender melhor.
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    //Product page
   } else if (pathName === "/product") {
     res.end("This is the product!");
+
+    //API
   } else if (pathName === "/api") {
     fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
       res.writeHead(200, { "Content-type": "application/json" });
       res.end(data);
     });
+
+    //Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
